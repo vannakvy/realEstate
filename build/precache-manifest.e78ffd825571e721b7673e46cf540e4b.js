216e22daa22b936d332288315f0d5ae704a777df
jrnn21 <chamroeuncl22@gmail.com>
2021-08-04 21:25:51 +0700

    dd

---
+++ build/precache-manifest.e78ffd825571e721b7673e46cf540e4b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba82eadcc4ceed777a3aeeb5b92a730",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeafa6fda08afdc09d4e",
+    "url": "/static/css/2.9ff5e96c.chunk.css"
+  },
+  {
+    "revision": "851b55d9d954a1e98e16",
+    "url": "/static/css/main.565b3913.chunk.css"
+  },
+  {
+    "revision": "eeafa6fda08afdc09d4e",
+    "url": "/static/js/2.ae1518c6.chunk.js"
+  },
+  {
+    "revision": "c7adc375d0db3d8ed487a4e11f660935",
+    "url": "/static/js/2.ae1518c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "851b55d9d954a1e98e16",
+    "url": "/static/js/main.7e1848ee.chunk.js"
+  },
+  {
+    "revision": "789ac8c282e46d696f70",
+    "url": "/static/js/runtime-main.433ed129.js"
+  },
+  {
+    "revision": "7b996be20a59b464bc170ecb6bebca39",
+    "url": "/static/media/KhmerOSmuollight.7b996be2.ttf"
+  },
+  {
+    "revision": "d586f04d96bedb5b1fceac44f77ad2d3",
+    "url": "/static/media/KhmerOSsiemreap.d586f04d.ttf"
+  },
+  {
+    "revision": "f40cb87f7112b9805bfcd049489ae2f3",
+    "url": "/static/media/TACTENG.f40cb87f.TTF"
+  },
+  {
+    "revision": "77bcf725aa4ef44aadd780668117a331",
+    "url": "/static/media/covid19.77bcf725.png"
+  },
+  {
+    "revision": "6e8459153cb716abf1f9345a18d9ea57",
+    "url": "/static/media/death.6e845915.png"
+  },
+  {
+    "revision": "282675630a5256d9bec382797744c6f6",
+    "url": "/static/media/goglobalschool.28267563.png"
+  },
+  {
+    "revision": "9b14bdb33cd43eb617d4c8d38fef25e6",
+    "url": "/static/media/logo-company.9b14bdb3.png"
+  },
+  {
+    "revision": "5cb2be8b86986a28443d224e30e1b3c5",
+    "url": "/static/media/recover.5cb2be8b.png"
+  },
+  {
+    "revision": "8b8e805f363960265540b85d674cb339",
+    "url": "/static/media/spritesheet.8b8e805f.svg"
+  },
+  {
+    "revision": "7a6f946fd5e8c73fc27edd80693d26d4",
+    "url": "/static/media/srLogo.7a6f946f.png"
+  }
+]);
